9ead61f823ee3a08df0cc24d75d2f39e392a0671
Thorsten Kober <thorstenk@google.com>
2024-03-08 11:00:45 -0500

    use ssr false

--- apps/news-site/news-site-nuxt/nuxt.config.ts
+++ apps/news-site/news-site-nuxt/nuxt.config.ts
@@ -27,6 +27,7 @@ const headConfig = {
 };
 
 const baseConfig = {
+    ssr: false,
     css: ["news-site-css/dist/variables.css", "news-site-css/dist/global.css", "news-site-css/dist/a11y.css", "news-site-css/dist/icons.css", "news-site-css/dist/text.css"],
     components: ["~/components", "~/components/assets", "~/components/atoms", "~/components/molecules", "~/components/organisms"],
     router: {

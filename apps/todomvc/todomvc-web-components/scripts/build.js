8ddfc8419592d332269f46af2b4e98f2678abd43
Thorsten Kober <thorstenk@google.com>
2024-03-23 10:37:33 -0400

    wc

--- apps/todomvc/todomvc-web-components/scripts/build.js
+++ apps/todomvc/todomvc-web-components/scripts/build.js
@@ -1,94 +1,95 @@
 const fs = require("fs").promises;
-const getDirName = require("path").dirname;
+const { dirname } = require("path");
 
-const rootDirectory = "./";
-const sourceDirectory = "./src";
-const targetDirectory = "./dist";
+const filesToMove = [
+    { src: "index.html", dest: "./dist/index.html" },
+    { src: "favicon.ico", dest: "./dist/favicon.ico" },
+    { src: "benchmark-connector.min.js", dest: "./dist/benchmark-connector.min.js" },
+    { src: "node_modules/todomvc-css/dist/global.css", dest: "./dist/styles/global.css" },
+    { src: "node_modules/todomvc-css/dist/header.css", dest: "./dist/styles/header.css" },
+    { src: "node_modules/todomvc-css/dist/footer.css", dest: "./dist/styles/footer.css" },
+    { src: "node_modules/todomvc-css/dist/global.constructable.js", dest: "./dist/styles/global.constructable.js" },
+    { src: "node_modules/todomvc-css/dist/app.constructable.js", dest: "./dist/styles/app.constructable.js" },
+    { src: "node_modules/todomvc-css/dist/topbar.constructable.js", dest: "./dist/styles/topbar.constructable.js" },
+    { src: "node_modules/todomvc-css/dist/main.constructable.js", dest: "./dist/styles/main.constructable.js" },
+    { src: "node_modules/todomvc-css/dist/bottombar.constructable.js", dest: "./dist/styles/bottombar.constructable.js" },
+    { src: "node_modules/todomvc-css/dist/todo-list.constructable.js", dest: "./dist/styles/todo-list.constructable.js" },
+    { src: "node_modules/todomvc-css/dist/todo-item.constructable.js", dest: "./dist/styles/todo-item.constructable.js" },
+];
 
-const htmlFile = "index.html";
+const importsToRename = [
+    {
+        src: "node_modules/todomvc-css/dist/",
+        dest: "styles/",
+        files: [ "./dist/index.html" ]
+    },
+    {
+        src: "src/",
+        dest: "",
+        files: [ "./dist/index.html" ]
+    },
+    {
+        src: "../../../node_modules/todomvc-css/dist/",
+        dest: "../../styles/",
+        files: [
+            "./dist/components/todo-app/todo-app.component.js",
+            "./dist/components/todo-bottombar/todo-bottombar.component.js",
+            "./dist/components/todo-item/todo-item.component.js",
+            "./dist/components/todo-list/todo-list.component.js",
+            "./dist/components/todo-topbar/todo-topbar.component.js",
+        ],
+    }
+]
 
-const filesToMove = {
-    index: [
-        { src: "node_modules/todomvc-css/dist/global.css", dest: "styles/global.css" },
-        { src: "node_modules/todomvc-css/dist/header.css", dest: "styles/header.css" },
-        { src: "node_modules/todomvc-css/dist/footer.css", dest: "styles/footer.css" },
-        { src: "favicon.ico", dest: "favicon.ico" },
-        { src: "benchmark-connector.min.js", dest: "benchmark-connector.min.js" }
-    ],
-    app: [
-        { src: "node_modules/todomvc-css/dist/global.constructable.js", dest: "styles/global.constructable.js" },
-        { src: "node_modules/todomvc-css/dist/app.constructable.js", dest: "styles/app.constructable.js" },
-        { src: "node_modules/todomvc-css/dist/topbar.constructable.js", dest: "styles/topbar.constructable.js" },
-        { src: "node_modules/todomvc-css/dist/main.constructable.js", dest: "styles/main.constructable.js" },
-        { src: "node_modules/todomvc-css/dist/bottombar.constructable.js", dest: "styles/bottombar.constructable.js" },
-        { src: "node_modules/todomvc-css/dist/todo-list.constructable.js", dest: "styles/todo-list.constructable.js" },
-        { src: "node_modules/todomvc-css/dist/todo-item.constructable.js", dest: "styles/todo-item.constructable.js" },
-    ],
-};
+async function createDirectory(directory) {
+    await fs.rm(directory, { recursive: true, force: true });
+    await fs.mkdir(directory);
+}
 
-const importsToRename = {
-    src: "../../../node_modules/todomvc-css/dist/",
-    dest: "../../styles/",
-    files: [
-        "components/todo-app/todo-app.component.js",
-        "components/todo-bottombar/todo-bottombar.component.js",
-        "components/todo-item/todo-item.component.js",
-        "components/todo-list/todo-list.component.js",
-        "components/todo-topbar/todo-topbar.component.js",
-    ],
-};
-
-const copy = async (src, dest) => {
-    await fs.mkdir(getDirName(dest), { recursive: true });
-    await fs.copyFile(src, dest);
-};
-
-const copyFilesToMove = async (files) => {
-    for (let i = 0; i < files.length; i++)
-        await copy(files[i].src, `${targetDirectory}/${files[i].dest}`);
-};
-
-const updateImports = async ({ file, src, dest }) => {
-    let contents = await fs.readFile(`${targetDirectory}/${file}`, "utf8");
-    contents = contents.replaceAll(src, dest);
-    await fs.writeFile(`${targetDirectory}/${file}`, contents);
-};
-
-const build = async () => {
-    // remove dist directory if it exists
-    await fs.rm(targetDirectory, { recursive: true, force: true });
-
-    // re-create the directory.
-    await fs.mkdir(targetDirectory);
-
-    // copy src folder
-    await fs.cp(sourceDirectory, targetDirectory, { recursive: true }, (err) => {
+async function copyDirectory(src, dest) {
+    await fs.cp(src, dest, { recursive: true }, (err) => {
         if (err)
             console.error(err);
     });
+}
+
+async function copyFile(src, dest) {
+    await fs.mkdir(dirname(dest), { recursive: true });
+    await fs.copyFile(src, dest);
+};
+
+async function copyFiles(files) {
+    for (const file of files)
+        await copyFile(file.src, `${file.dest}`);
+};
+
+async function updateImportsInFile({ file, src, dest }) {
+    let contents = await fs.readFile(`${file}`, "utf8");
+    contents = contents.replaceAll(src, dest);
+    await fs.writeFile(`${file}`, contents);
+};
+
+async function updateImports({ files, src, dest }) {
+    for (const file of files) {
+        await updateImportsInFile({ file, src, dest })
+    }
+}
+
+const build = async () => {
+    // create dist folder
+    await createDirectory("./dist");
+
+    // copy src folder
+    await copyDirectory("./src", "./dist");
 
     // copy files to Move
-    for (const key in filesToMove)
-        copyFilesToMove(filesToMove[key]);
+    await copyFiles(filesToMove);
 
-    // read html file
-    let contents = await fs.readFile(`${rootDirectory}/${htmlFile}`, "utf8");
-
-    // remove base paths from files to move
-    const filesToMoveForIndex = filesToMove.index;
-    for (let i = 0; i < filesToMoveForIndex.length; i++)
-        contents = contents.replace(filesToMoveForIndex[i].src, filesToMoveForIndex[i].dest);
-
-    // remove basePath from source directory
-    const basePath = `${sourceDirectory.split("/")[1]}/`;
-    const re = new RegExp(basePath, "g");
-    contents = contents.replace(re, "");
-
-    // write html files
-    await fs.writeFile(`${targetDirectory}/${htmlFile}`, contents);
-
-    // rename imports in modules
-    importsToRename.files.forEach((file) => updateImports({ file, src: importsToRename.src, dest: importsToRename.dest }));
+    // rename imports files
+    for (const entry of importsToRename){
+        const { files, src, dest } = entry;
+        await updateImports({ files, src, dest });
+    }
 
     console.log("done!!");
 };

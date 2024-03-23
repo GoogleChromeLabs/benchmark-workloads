6a31150a79420513b74fb72163602f50ec827a29
Thorsten Kober <thorstenk@google.com>
2024-03-23 11:28:52 -0400

    test

--- apps/todomvc/todomvc-web-components/scripts/build.js
+++ apps/todomvc/todomvc-web-components/scripts/build.js
@@ -1,5 +1,4 @@
-const fs = require("fs").promises;
-const { dirname } = require("path");
+import { createDirectory, copyDirectory, copyFiles, updateImports } from "app-build-scripts";
 
 const filesToMove = [
     { src: "index.html", dest: "./dist/index.html" },
@@ -40,40 +39,6 @@ const importsToRename = [
         ],
     }
 ]
-
-async function createDirectory(directory) {
-    await fs.rm(directory, { recursive: true, force: true });
-    await fs.mkdir(directory);
-}
-
-async function copyDirectory(src, dest) {
-    await fs.cp(src, dest, { recursive: true }, (err) => {
-        if (err)
-            console.error(err);
-    });
-}
-
-async function copyFile(src, dest) {
-    await fs.mkdir(dirname(dest), { recursive: true });
-    await fs.copyFile(src, dest);
-};
-
-async function copyFiles(files) {
-    for (const file of files)
-        await copyFile(file.src, `${file.dest}`);
-};
-
-async function updateImportsInFile({ file, src, dest }) {
-    let contents = await fs.readFile(`${file}`, "utf8");
-    contents = contents.replaceAll(src, dest);
-    await fs.writeFile(`${file}`, contents);
-};
-
-async function updateImports({ files, src, dest }) {
-    for (const file of files) {
-        await updateImportsInFile({ file, src, dest })
-    }
-}
 
 const build = async () => {
     // create dist folder

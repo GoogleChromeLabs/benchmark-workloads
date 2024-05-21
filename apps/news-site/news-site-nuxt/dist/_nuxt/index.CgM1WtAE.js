c120ce7ff30fc6de008c6ecb7ca7543ad3da41a4
Thorsten Kober <tkober@gmail.com>
2024-05-21 17:41:43 -0400

    build apps

---
+++ apps/news-site/news-site-nuxt/dist/_nuxt/index.CgM1WtAE.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./Page.CyVcxt5T.js";import{u as s,g as c,o as n}from"./entry.D1JuWpR3.js";const i={__name:"index",setup(t){return s({script:[{src:"./benchmark-connector.min.js",tagPosition:"bodyClose"}]}),(a,r)=>{const o=e;return n(),c(o,{id:"home"})}}};export{i as default};
+//# sourceMappingURL=index.CgM1WtAE.js.map
---
+++ apps/news-site/news-site-nuxt/dist/_nuxt/politics.BsOcU0Wd.js
@@ -0,0 +1,2 @@
+import{_ as s}from"./Page.CyVcxt5T.js";import{u as c,g as t,o as e}from"./entry.D1JuWpR3.js";const p={__name:"politics",setup(a){return c({script:[{src:"./benchmark-connector.min.js",tagPosition:"bodyClose"}]}),(n,r)=>{const o=s;return e(),t(o,{id:"politics"})}}};export{p as default};
+//# sourceMappingURL=politics.BsOcU0Wd.js.map

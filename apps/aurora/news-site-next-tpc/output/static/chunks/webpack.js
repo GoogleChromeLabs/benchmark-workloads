9bd8d275902c1d54f85fb6bd025268649c2974e6
Thorsten Kober <tkober@gmail.com>
2024-06-07 15:47:29 -0400

    add analytics

--- apps/aurora/news-site-next-tpc/output/static/chunks/webpack.js
+++ apps/aurora/news-site-next-tpc/output/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8bc636c9f0143b3f"; }
+/******/ 		__webpack_require__.h = function() { return "9bd3b8bb52ca40f3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

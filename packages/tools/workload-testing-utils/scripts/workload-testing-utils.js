2b79fbf2ad926f8afb02c34e7c38482f992631c5
Thorsten Kober <tkober@gmail.com>
2024-07-15 16:10:12 -0400

    cleanup utils

--- packages/tools/workload-testing-utils/scripts/workload-testing-utils.js
+++ packages/tools/workload-testing-utils/scripts/workload-testing-utils.js
@@ -1,73 +1,74 @@
 /**
  * BenchmarkTestStep
- * 
+ *
  * A single test step, with a common interface to interact with.
  */
 export class BenchmarkTestStep {
-  constructor(name, fn) {
-      this.name = name;
-      this.run = fn;
-  }
+    constructor(name, fn) {
+        this.name = name;
+        this.run = fn;
+    }
 }
 
 /**
  * BenchmarkTestSuite
- * 
+ *
  * A single test suite that contains one or more test steps.
  */
 export class BenchmarkTestSuite {
-  constructor(name, tests) {
-    this.name = name;
-    this.tests = tests;
-  }
+    constructor(name, tests) {
+        this.name = name;
+        this.tests = tests;
+    }
 
-  async run() {
-    for (const test of this.tests)
-          await test.run();
-  
-      return "done";
+    async run() {
+        for (const test of this.tests) test.run();
+
+        return "done";
     }
 }
 
 /**
  * BenchmarkTestManager
- * 
+ *
  * A collection of test suites for a single workload.
  */
 export class BenchmarkTestManager {
-  constructor(name, suites) {
-    this.name = name;
-    this.suites = suites;
-  }
-
-  async run(suitesToRun) {
-    console.log(`Starting tests for ${this.name}`);
-    const selectedSuites = !suitesToRun ? [...this.suites] : this.suites.filter(suite => suitesToRun.includes(suite.name));
-
-    for (const suite of selectedSuites) {
-        console.log(`Starting ${suite.name} test.`);
-        await suite.run();
-        console.log(`Completed ${suite.name} test.`);
+    constructor(name, suites) {
+        this.name = name;
+        this.suites = suites;
     }
 
-    return `Done with tests for ${this.name}`;
-  }
+    async run(suitesToRun) {
+        console.log(`Starting tests for ${this.name}`);
+        const selectedSuites = !suitesToRun
+            ? [...this.suites]
+            : this.suites.filter((suite) => suitesToRun.includes(suite.name));
+
+        for (const suite of selectedSuites) {
+            console.log(`Starting ${suite.name} test.`);
+            await suite.run();
+            console.log(`Completed ${suite.name} test.`);
+        }
+
+        return `Done with tests for ${this.name}`;
+    }
 }
 
 /**
  * runWorkloadTest
- * 
+ *
  * A function that wraps a test function in a promise.
- * 
+ *
  * @param {*} test - A function that performs an interaction in the app.
  * @return {Promise} A promise to ensure test runs wait for test functions to complete.
  */
 export function runWorkloadTest(test) {
-  const delay = window.testStepDelay ?? 0;
-  return new Promise((resolve) => {
-    test();
-    setTimeout(() => resolve(), delay);
-  });
+    const delay = window.testStepDelay ?? 0;
+    return new Promise((resolve) => {
+        test();
+        setTimeout(() => resolve(), delay);
+    });
 }
 
 /**
@@ -75,18 +76,24 @@ export function runWorkloadTest(test) {
  */
 
 export function forceLayoutWaitFor() {
-  return new Promise((resolve) => {
-    const rect = document.body.getBoundingClientRect();
-    const e = document.elementFromPoint((rect.width / 2) | 0, (rect.height / 2) | 0);
-    resolve(e);
-  });
+    return new Promise((resolve) => {
+        const rect = document.body.getBoundingClientRect();
+        const e = document.elementFromPoint(
+            (rect.width / 2) | 0,
+            (rect.height / 2) | 0
+        );
+        resolve(e);
+    });
 }
 
 /**
  * foreLayout;
  */
 export function forceLayout() {
-  const rect = document.body.getBoundingClientRect();
-  const e = document.elementFromPoint((rect.width / 2) | 0, (rect.height / 2) | 0);
-  return e;
+    const rect = document.body.getBoundingClientRect();
+    const e = document.elementFromPoint(
+        (rect.width / 2) | 0,
+        (rect.height / 2) | 0
+    );
+    return e;
 }

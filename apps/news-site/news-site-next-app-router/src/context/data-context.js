e1ab8897b4f4b09feae278d2bc79e7bee3106800
Thorsten Kober <tkober@gmail.com>
2024-07-26 17:16:12 -0400

    fix provider

--- apps/news-site/news-site-next-app-router/src/context/data-context.js
+++ apps/news-site/news-site-next-app-router/src/context/data-context.js
@@ -1,7 +1,8 @@
 "use client";
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useLayoutEffect } from "react";
 import { dataSource } from "../data";
+import { useSearchParams } from "next/navigation";
 
 const RTL_LOCALES = ["ar", "he", "fa", "ps", "ur"];
 const DEFAULT_LANG = "en";
@@ -10,13 +11,16 @@ const DEFAULT_DIR = "ltr";
 const DataContext = createContext(null);
 
 export const DataContextProvider = ({ children }) => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const langFromUrl = urlParams.get("lang")?.toLowerCase();
+
+    const searchParams = useSearchParams();
+    const langFromUrl = searchParams.get("lang")?.toLowerCase();
     const lang = langFromUrl && langFromUrl in dataSource ? langFromUrl : DEFAULT_LANG;
     const dir = lang && RTL_LOCALES.includes(lang) ? "rtl" : DEFAULT_DIR;
 
-    document.documentElement.setAttribute("dir", dir);
-    document.documentElement.setAttribute("lang", lang);
+    useLayoutEffect(() => {
+        document.documentElement.setAttribute("dir", dir);
+        document.documentElement.setAttribute("lang", lang);
+    }, []);
 
     const value = {
         lang,

19f4be1727ccdbb81b527fa7f1a1e06aa9db9082
Thorsten Kober <tkober@gmail.com>
2024-07-31 13:16:12 -0400

    add test

--- examples/hello-world/dist/workload.test.js
+++ examples/hello-world/dist/workload.test.js
@@ -1,6 +1,22 @@
-import { BenchmarkTestStep, BenchmarkTestSuite, BenchmarkTestManager, forceLayout } from "./workload-testing-utils.min.js";
+import { BenchmarkTestStep, BenchmarkTestSuite, BenchmarkTestManager } from "./workload-testing-utils.min.js";
 
-console.log(BenchmarkTestManager);
-console.log(BenchmarkTestStep);
-console.log(BenchmarkTestSuite);
-console.log(forceLayout);
+window.benchmarkTestManager = new BenchmarkTestManager(window.name, [
+    new BenchmarkTestSuite("Counter", [
+        new BenchmarkTestStep("Increase by 10", () => {
+            const increaseButton = document.querySelector("#increase-btn");
+            for (let i = 0; i < 10; i++) {
+                increaseButton.click();
+            }
+        }),
+        new BenchmarkTestStep("Decrease by 5", () => {
+            const decreaseButton = document.querySelector("#decrease-btn");
+            for (let i = 0; i < 5; i++) {
+                decreaseButton.click();
+            }
+        }),
+        new BenchmarkTestStep("Reset", () => {
+            const resetButton = document.querySelector("#reset-btn");
+            resetButton.click();
+        }),
+    ])
+]);

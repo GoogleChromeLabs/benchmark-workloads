b120b119b586b8f7166dfc00df5666ab4c4290d7
Thorsten Kober <tkober@gmail.com>
2024-08-01 14:02:26 -0400

    separate runs

--- packages/tools/workload-testing-utils/scripts/workload-testing-utils.js
+++ packages/tools/workload-testing-utils/scripts/workload-testing-utils.js
@@ -4,89 +4,103 @@
  * A single test step, with a common interface to interact with.
  */
 export class BenchmarkTestStep {
-  constructor(name, fn) {
-      this.name = name;
-      this.run = fn;
-  }
+    constructor(name, fn) {
+        this.name = name;
+        this.fn = fn;
+    }
+
+    run() {
+        this.fn();
+    }
+
+    async runAsync(delay = 0) {
+        return new Promise((resolve) => {
+            this.fn();
+            setTimeout(() => resolve(), delay);
+        });
+    }
 }
 
 /**
-* BenchmarkTestSuite
-*
-* A single test suite that contains one or more test steps.
-*/
+ * BenchmarkTestSuite
+ *
+ * A single test suite that contains one or more test steps.
+ */
 export class BenchmarkTestSuite {
-  constructor(name, tests) {
-      this.name = name;
-      this.tests = tests;
-  }
+    constructor(name, tests) {
+        this.name = name;
+        this.tests = tests;
+    }
 
-  getTestByName(name) {
-      return this.tests.find(test => test.name === name);
-  }
+    getTestByName(name) {
+        return this.tests.find((test) => test.name === name);
+    }
 
-  async run() {
-      for (const test of this.tests) test.run();
+    run() {
+        for (const test of this.tests) test.run();
+        return { type: "suite-tests-complete", status: "success" };
+    }
 
-      return { type: "suite-tests-complete", status: "success" };
-  }
+    async runAsync() {
+        for (const test of this.tests) await test.runAsync();
+        return { type: "suite-tests-complete", status: "success" };
+    }
 }
 
 /**
-* BenchmarkTestManager
-*
-* A collection of test suites for a single workload.
-*/
+ * BenchmarkTestManager
+ *
+ * A collection of test suites for a single workload.
+ */
 export class BenchmarkTestManager {
-  constructor(name, suites) {
-      this.name = name;
-      this.suites = suites;
-  }
+    constructor(name, suites) {
+        this.name = name;
+        this.suites = suites;
+    }
 
-  getSuiteByName(name) {
-      return this.suites.find(suite => suite.name === name);
-  }
+    getSuiteByName(name) {
+        return this.suites.find((suite) => suite.name === name);
+    }
 
-  async run(suitesToRun) {
-      console.log(`Starting tests for ${this.name}`);
-      const selectedSuites = !suitesToRun
-          ? [...this.suites]
-          : this.suites.filter((suite) => suitesToRun.includes(suite.name));
+    run(suitesToRun) {
+        console.log(`Starting tests for ${this.name}`);
+        const selectedSuites = !suitesToRun
+            ? [...this.suites]
+            : this.suites.filter((suite) => suitesToRun.includes(suite.name));
 
-      for (const suite of selectedSuites) {
-          console.log(`Starting ${suite.name} test.`);
-          await suite.run();
-          console.log(`Completed ${suite.name} test.`);
-      }
+        for (const suite of selectedSuites) {
+            console.log(`Starting ${suite.name} test.`);
+            suite.run();
+            console.log(`Completed ${suite.name} test.`);
+        }
 
-      return `Done with tests for ${this.name}`;
-  }
+        return `Done with tests for ${this.name}`;
+    }
+
+    async runAsync(suitesToRun) {
+        console.log(`Starting tests for ${this.name}`);
+        const selectedSuites = !suitesToRun
+            ? [...this.suites]
+            : this.suites.filter((suite) => suitesToRun.includes(suite.name));
+
+        for (const suite of selectedSuites) {
+            console.log(`Starting ${suite.name} test.`);
+            await suite.runAsync();
+            console.log(`Completed ${suite.name} test.`);
+        }
+
+        return `Done with tests for ${this.name}`;
+    }
 }
 
 /**
-* runWorkloadTest
-*
-* A function that wraps a test function in a promise.
-*
-* @param {*} test - A function that performs an interaction in the app.
-* @return {Promise} A promise to ensure test runs wait for test functions to complete.
-*/
-export function runWorkloadTest(test) {
-  const delay = window.testStepDelay ?? 0;
-  return new Promise((resolve) => {
-      test();
-      setTimeout(() => resolve(), delay);
-  });
-}
-
-/**
-* foreLayout
-*/
+ * foreLayout
+ */
 export function forceLayout() {
-  const rect = document.body.getBoundingClientRect();
-  const e = document.elementFromPoint(
-      (rect.width / 2) | 0,
-      (rect.height / 2) | 0
-  );
-  return e;
+    const rect = document.body.getBoundingClientRect();
+    const e = document.elementFromPoint(
+        (rect.width / 2) | 0,
+        (rect.height / 2) | 0
+    );
+    return e;
 }

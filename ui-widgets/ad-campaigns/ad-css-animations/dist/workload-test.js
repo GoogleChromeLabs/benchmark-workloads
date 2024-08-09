b6735e86b6dca4ae26a932f4dc98c4746f4b6bdd
Thorsten Kober <tkober@gmail.com>
2024-08-09 15:44:13 -0400

    more builds

--- ui-widgets/ad-campaigns/ad-css-animations/dist/workload-test.js
+++ ui-widgets/ad-campaigns/ad-css-animations/dist/workload-test.js
@@ -1,13 +1,13 @@
-import { BenchmarkTestStep, BenchmarkTestSuite, BenchmarkTestManager } from "./workload-testing-utils.min.js";
+import { BenchmarkTestStep, BenchmarkTestSuite, BenchmarkTestManager, getElement } from "./workload-testing-utils.min.js";
 
 window.benchmarkTestManager = new BenchmarkTestManager(window.name, [
     new BenchmarkTestSuite("Ad", [
         new BenchmarkTestStep("Clickthrough", () => {
-            const button = document.querySelector(".clickthrough");
+            const button = getElement(".clickthrough");
             button.click();
         }),
         new BenchmarkTestStep("Action", () => {
-            const button = document.querySelector("#action");
+            const button = getElement("#action");
             button.click();
         }),
     ])

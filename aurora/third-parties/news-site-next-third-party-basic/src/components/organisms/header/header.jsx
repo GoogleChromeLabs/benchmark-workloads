317f416b105190592b51f9126b33cc269e14351f
Thorsten Kober <tkober@gmail.com>
2024-08-20 08:50:40 -0400

    tpc basic

---
+++ aurora/third-parties/news-site-next-third-party-basic/src/components/organisms/header/header.jsx
@@ -0,0 +1,14 @@
+import TitleIcon from "@/components/assets/title-icon";
+
+import styles from "news-site-css/dist/header.module.css";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header className={styles["page-header"]}>
+            <NavLink to="/" className={styles["page-header-title"]}>
+                <TitleIcon />
+            </NavLink>
+        </header>
+    );
+}

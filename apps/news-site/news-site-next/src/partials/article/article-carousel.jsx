1695028845542e08cc23d3d08e0979a5ff655e3e
Thorsten Kober <tkober@gmail.com>
2024-10-18 15:29:55 -0400

    simple implementation

--- apps/news-site/news-site-next/src/partials/article/article-carousel.jsx
+++ apps/news-site/news-site-next/src/partials/article/article-carousel.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable curly */
+import { useState, useEffect } from "react";
 import classNames from "classnames";
 import Image from "next/image";
 import ArticleTag from "@/components/molecules/article/article-tag";
@@ -12,23 +14,50 @@ export default function ArticleCarousel({ data, imageClass, meta }) {
     if (!data)
         return null;
 
+    const [direction, setDirection] = useState("right-to-left");
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [prevIndex, setPrevIndex] = useState(data.images.length - 1);
+    const [nextIndex, setNextIndex] = useState(1);
+
     const imageDir = process.env.TARGET && process.env.TARGET === "static" ? "./" : "/";
     const aspectRatio = data.width / data.height;
 
+    useEffect(() => {
+        console.log("direction", direction);
+        console.log("prevIndex", prevIndex);
+        console.log("currentIndex", currentIndex);
+        console.log("nextIndex", nextIndex);
+        console.log("***");
+    }, [currentIndex, nextIndex, prevIndex]);
+
+    function animate(dir) {
+        setDirection(dir === "prev" ? "left-to-right" : "right-to-left");
+
+        if (dir === "prev") {
+            setPrevIndex(prevIndex === 0 ? data.images.length - 1 : prevIndex - 1);
+            setCurrentIndex(currentIndex === 0 ? data.images.length - 1 : currentIndex - 1);
+            setNextIndex(nextIndex === 0 ? data.images.length - 1 : nextIndex - 1);
+        } else {
+            setPrevIndex(prevIndex === data.images.length - 1 ? 0 : prevIndex + 1);
+            setCurrentIndex(currentIndex === data.images.length - 1 ? 0 : currentIndex + 1);
+            setNextIndex(nextIndex === data.images.length - 1 ? 0 : nextIndex + 1);
+        }
+    }
+
     function prev() {
-        console.log("prev");
+        animate("prev");
     }
 
     function next() {
-        console.log("next");
+        animate("next");
     }
 
     return (
         <div className={carouselStyles.container} style={{ aspectRatio }}>
             <div className={carouselStyles.content}>
                 {
-                    data.images.map(image => <div className={imageClass} key={image.id} >
-                        <Image className={styles["article-image"]} src={`${imageDir}${image.src}`} width={image.width} height={image.height} style={{ width: "100%", height: "auto", position: "absolute" }} alt={image.alt} />
+                    data.images.map((image, index) => <div className={classNames(imageClass, carouselStyles.image, carouselStyles[direction], { [carouselStyles["prev-slide"]]: index === prevIndex, [carouselStyles["next-slide"]]: index === nextIndex, [carouselStyles["current-slide"]]: index === currentIndex })} key={image.id} >
+                        <Image key={image.id} className={classNames(styles["article-image"])} src={`${imageDir}${image.src}`} width={image.width} height={image.height} alt={image.alt} />
                     </div>)
                 }
             </div>

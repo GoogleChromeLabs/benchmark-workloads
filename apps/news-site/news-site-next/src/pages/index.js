46c6e9a99a80f839a558c0fc8370830c88f95987
Thorsten Kober <tkober@gmail.com>
2024-12-23 13:40:40 -0500

    update comments

--- apps/news-site/news-site-next/src/pages/index.js
+++ apps/news-site/news-site-next/src/pages/index.js
@@ -9,8 +9,9 @@ import suites, { appName, appVersion } from "@/workload-test.mjs";
 export default function App() {
     useEffect(() => {
         /*
-        window.addEventListener("message", (event) => console.log(event.data));
-        window.postMessage({ id: "news-next-1", key: "benchmark-connector", type: "benchmark-suite", name: "default" }, "*");
+            Paste below into dev console for manual testing:
+            window.addEventListener("message", (event) => console.log(event.data));
+            window.postMessage({ id: "news-site-next-1.0.0", key: "benchmark-connector", type: "benchmark-suite", name: "default" }, "*");
         */
         const benchmarkConnector = new BenchmarkConnector(suites, appName, appVersion);
         benchmarkConnector.connect();
